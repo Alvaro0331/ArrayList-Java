ae40e265834a9de93d73d7e6d1a0f967578713e2
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2025-02-15 02:04:19 -0600

    Creada la estructura y algunos metodos de la lista

--- src/Lista.java
+++ src/Lista.java
@@ -1,3 +1,44 @@
+
+import java.util.ArrayList;
+
 public class Lista {
+    private ArrayList<Nodo> list;
     
+    /*Constructor*/
+    public Lista(ArrayList<Nodo> list){
+        this.list=list;
+    }
+    
+    public void agregarLista(Nodo nodo){
+        this.list.add(nodo);
+    }
+    
+    public ArrayList<Nodo> getActualizar(){
+        return this.list;
+    }
+    
+    public Nodo Buscar(Nodo nodo){
+        Nodo aux=null;
+        
+        /*Recorrer la lista*/
+        for(Nodo n:this.list){
+            if(nodo.getID()==n.getID()){
+                aux=new Nodo();
+                aux.setID((n.getID()));
+                aux.setNombre(n.getNombre());
+            }
+        }
+        return aux;
+    }
+    
+    public void editar(){
+        
+    }
+    public void remover(){
+        
+    }
+    
+    public int getNuevoId(){
+        return this.list.size()+1;
+    }
 }
