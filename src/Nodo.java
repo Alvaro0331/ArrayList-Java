9a3a09fe46f6099626893f7bb69c403ddc62c273
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2025-02-15 01:38:27 -0600

    creada estructura del nodo

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,12 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author aldag
- */
 public class Nodo {
+    private int ID;
+    private String nombre;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
